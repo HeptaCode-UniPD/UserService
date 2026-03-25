b9b5c8ada3d093fb43471a8813b63a87e482217f
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-03-25 19:47:51 +0100

    added UserService

---
+++ src/user/application/user.service.ts
@@ -0,0 +1,45 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { UserEntity } from "../domain/user.entity";
+import { ValidatedUserDataDTO } from "../../ingestion/dto/validated-user-data.dto";
+import type { IUserRepository } from "./ports/user.repository.interface";
+import { USER_REPOSITORY } from "./ports/user.repository.interface";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @Inject(USER_REPOSITORY)
+        private readonly userRepository: IUserRepository
+    ) {}
+
+    async register(data: ValidatedUserDataDTO): Promise<UserEntity> {
+        const exists = await this.userRepository.existsByEmail(data.email);
+        if (exists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await bcrypt.hash(data.password, 10);
+
+        const user = new UserEntity(
+            crypto.randomUUID(),
+            data.email,
+            passwordHash
+        );
+        return this.userRepository.save(user);
+    }
+
+    async login(data: ValidatedUserDataDTO): Promise<UserEntity> {
+        const user = await this.userRepository.findByEmail(data.email);
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        const isValid = await bcrypt.compare(data.password, user.passwordHash);
+        if (!isValid) throw new Error("Invalid credentials");
+
+        if (user.passwordHash !== data.password) {
+            throw new Error("Invalid credentials");
+        }
+        return user;
+    }
+}
