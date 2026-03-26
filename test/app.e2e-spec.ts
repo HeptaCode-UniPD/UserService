2a81ca4523040832bafba59ea95857fc9e9ee58d
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-03-26 22:00:03 +0100

    aggiunti test integrazione

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import * as mongoose from 'mongoose';
 
 describe('Auth (e2e)', () => {
     let app: INestApplication;
@@ -16,6 +17,10 @@ describe('Auth (e2e)', () => {
     });
 
     afterAll(async () => {
+        const collections = mongoose.connection.collections;
+        for (const key in collections) {
+            await collections[key].deleteMany({});
+        }
         await app.close();
     });
 
@@ -48,6 +53,52 @@ describe('Auth (e2e)', () => {
                 .post('/auth/login')
                 .send({ email: 'test@test.com', password: 'passwordsbagliata' })
                 .expect(401);
+        });
+
+        it('dovrebbe restituire 401 se utente non esiste', async () => {
+            return request(app.getHttpServer())
+                .post('/auth/login')
+                .send({ email: 'nonesiste@test.com', password: '12345678' })
+                .expect(401);
+        });
+    });
+
+    describe('POST /repo', () => {
+        it('dovrebbe aggiungere un repo', async () => {
+            return request(app.getHttpServer())
+                .post('/repo')
+                .send({ idUtente: 'user1', url: 'https://github.com/octocat/Hello-World' })
+                .expect(201);
+        });
+
+        it('dovrebbe restituire 400 se URL non valido', async () => {
+            return request(app.getHttpServer())
+                .post('/repo')
+                .send({ idUtente: 'user1', url: 'urlnonvalido' })
+                .expect(400);
+        });
+
+        it('dovrebbe restituire 400 se repo GitHub non esiste', async () => {
+            return request(app.getHttpServer())
+                .post('/repo')
+                .send({ idUtente: 'user1', url: 'https://github.com/utentechenoneesiste/repoinesistente123456' })
+                .expect(400);
+        });
+    });
+
+    describe('DELETE /repo', () => {
+        it('dovrebbe eliminare un repo', async () => {
+            return request(app.getHttpServer())
+                .delete('/repo')
+                .send({ idUtente: 'user1', url: 'https://github.com/octocat/Hello-World' })
+                .expect(200);
+        });
+
+        it('dovrebbe restituire 404 se repo non trovato', async () => {
+            return request(app.getHttpServer())
+                .delete('/repo')
+                .send({ idUtente: 'user1', url: 'https://github.com/owner/nonexistent' })
+                .expect(404);
         });
     });
 });
