94a7f9c85a0a1030d2036e8d60134e58c7b7ded2
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-03-26 19:56:16 +0100

    added repo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IngestionModule } from './ingestion/ingestion.module';
+import { RepoModule } from './repo/repo.module';
 
 @Module({
   imports: [
     // mi manca il nome del db guys quando arriva va aggiornato
     MongooseModule.forRoot(process.env.MONGO_URI!),
     IngestionModule,
+    RepoModule,
   ],
 })
 export class AppModule {}
---
+++ src/repo/repo.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { RepoPersistence, RepoSchema } from "./infrastructure/repo.schema";
+import { RepoMongoRepository } from "./infrastructure/repo.mongo.repository";
+import { RepoService } from "./application/repo.service";
+import { REPO_REPOSITORY } from "./application/ports/repo.repository.interface";
+import { GitHubAdapter } from "./infrastructure/github.adapter";
+import { GITHUB_SERVICE } from "./application/ports/github.service.interface";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: RepoPersistence.name, schema: RepoSchema }
+        ])
+    ],
+    providers: [
+        RepoService,
+        {
+            provide: REPO_REPOSITORY,
+            useClass: RepoMongoRepository,
+        },
+        {
+            provide: GITHUB_SERVICE,
+            useClass: GitHubAdapter,
+        }
+    ],
+    exports: [RepoService],
+})
+export class RepoModule {}
