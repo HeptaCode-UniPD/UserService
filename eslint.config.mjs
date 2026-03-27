28949a73d25606d68433b91c3537e0dc60c2c33c
AngelaFavaro <angela.favaro@studenti.unipd.it>
2026-03-27 09:34:09 +0100

    aggiunte metriche test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,32 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+
+      // --- Integrazione Metriche HeptaCode ---
+
+      // MPD07: Complessità Ciclomatica (Accettabile <= 15, Ottimale <= 10)
+      complexity: ['error', 15],
+
+      // MPD08: Parametri per metodo (Accettabile <= 6, Ottimale <= 4)
+      'max-params': ['error', 6],
+
+      // MPD09: Linee di codice per metodo (Accettabile < 35, Ottimale <= 20)
+      'max-lines-per-function': [
+        'warn',
+        { max: 35, skipBlankLines: true, skipComments: true },
+      ],
+
+      // MPD10: Linee di codice per file (Accettabile < 120, Ottimale <= 80)
+      'max-lines': [
+        'warn',
+        { max: 200, skipBlankLines: true, skipComments: true },
+      ],
+
+      // Altre regole per la Manutenibilità (Sezione 5.5.1)
+
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'no-console': 'warn',
     },
   },
 );
