5935adbe72c7162caf0507c086043184b9e5e4fa
angela <angela.canazza@studenti.unipd.it>
2026-04-01 23:29:37 +0200

    aggiornati path accettati per le richieste

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*', //non posso usare un dominio fisso perché cambia ad ogni aggiornamento del servizio
     methods: 'GET,POST,DELETE,PUT,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   });
